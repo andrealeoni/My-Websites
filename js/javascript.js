03850b1124d777c47b19ab3b39b550a230685bdd
andrealeoni <andrealeoni30@gmail.com>
2016-12-20 12:14:10 -0800

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+function function_01(){ window.location='websites/SITE_01/index.html';}
+function function_02(){ window.location='websites/SITE_02/index.html';}
+function function_03(){ window.location='websites/SITE_03/index.php';}
+function function_04(){ window.location='websites/SITE_04/index.html';}
+function function_05(){ window.location='websites/SITE_05/index.html';}
+function function_06(){ window.location='websites/SITE_06/index.html';}
